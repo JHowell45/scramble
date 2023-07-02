edc00bff9094797f2c29ce362c8eaafd9f17dbe7
Jacob <jacob@notactivelylooking.com>
2023-07-02 15:41:06 +0100

    Adding images and dates as well as a Player model.

--- backend/character/models.py
+++ backend/character/models.py
@@ -5,7 +5,20 @@ from django.db import models
 class Character(models.Model):
     name = models.CharField(max_length=100)
     description = models.TextField()
+    avatar = models.ImageField(
+        upload_to="characters/avatars/%Y/%m/%d/",
+        default="characters/avatars/default.jpg",
+    )
     strength = models.IntegerField()
     magic = models.IntegerField()
     dexterity = models.IntegerField()
-    health = models.IntegerField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+
+class Player(models.Model):
+    name = models.CharField(max_length=100)
+    character = models.ForeignKey(Character, on_delete=models.CASCADE)
+    current_health = models.IntegerField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
